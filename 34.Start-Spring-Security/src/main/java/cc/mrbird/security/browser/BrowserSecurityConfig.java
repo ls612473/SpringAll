749f908e6d5108cbfbc5f9ab72e217b704387daf
shun.li <shun.li@qizhidao.cn>
2020-01-10 20:36:16 +0800

    增加read.md文档

--- 34.Start-Spring-Security/src/main/java/cc/mrbird/security/browser/BrowserSecurityConfig.java
+++ 34.Start-Spring-Security/src/main/java/cc/mrbird/security/browser/BrowserSecurityConfig.java
@@ -12,8 +12,8 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.formLogin() // 表单登录
-         http.httpBasic() // HTTP Basic
+        http.formLogin() // 表单登录
+//         http.httpBasic() // HTTP Basic
                 .and()
                 .authorizeRequests() // 授权配置
                 .anyRequest()  // 所有请求
