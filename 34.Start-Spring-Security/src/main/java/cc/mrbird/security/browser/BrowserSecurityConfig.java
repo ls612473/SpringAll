efb572efb112c32eccdee4868dd17cc8c4b1af70
shun.li <shun.li@qizhidao.cn>
2020-01-10 17:32:50 +0800

    默认的用户及密码: user 密码：控制台打印

--- 34.Start-Spring-Security/src/main/java/cc/mrbird/security/browser/BrowserSecurityConfig.java
+++ 34.Start-Spring-Security/src/main/java/cc/mrbird/security/browser/BrowserSecurityConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ *  Using default security password: a002bb56-8f29-422c-8ff1-bcdcb8af3713 --- 来源于控制台
+ */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin() // 表单登录
-        // http.httpBasic() // HTTP Basic
+//        http.formLogin() // 表单登录
+         http.httpBasic() // HTTP Basic
                 .and()
                 .authorizeRequests() // 授权配置
                 .anyRequest()  // 所有请求
